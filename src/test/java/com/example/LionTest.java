b6d5c0ff6bc78c9bb90fae8f55c75aabb00735cd
Georgiy Pronin <proningeorgiy@yandex.ru>
2024-09-20 14:53:41 +0300

    Version 3

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -2,52 +2,30 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
-    private final String sex;
-    private final boolean hasMane;
-
-    Feline feline = Mockito.mock(Feline.class);
-
-    public LionTest(String sex, boolean hasMane) {
-        this.sex = sex;
-        this.hasMane = hasMane;
-    }
-
-
-    @Parameterized.Parameters
-    public static Object[][] FAQs() {
-        return new Object[][]{
-                {"Самец", true},
-                {"Самка", false},
-        };
-    }
+    @Mock
+    Feline feline;
 
     @Test
     public void getKittens() throws Exception {
-        Lion lion = new Lion(sex, feline);
+        Lion lion = new Lion("Самец", feline);
         lion.getKittens();
 
         Mockito.verify(feline).getKittens();
     }
 
     @Test
-    public void doesHaveMane() throws Exception {
-        Lion lion = new Lion(sex, feline);
-
-        assertEquals(hasMane, lion.doesHaveMane());
-    }
-
-    @Test
     public void getFood() throws Exception {
-        Lion lion = new Lion(sex, feline);
+        Lion lion = new Lion("Самец", feline);
         lion.getFood();
 
         Mockito.verify(feline).getFood("Хищник");
